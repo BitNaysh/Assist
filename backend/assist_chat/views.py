587d14728a84a3fe98e1c640868751d43f9ad014
Sanket Bhat <sanketbhat882002@gmail.com>
2023-04-20 20:04:16 +0530

    django

--- backend/assist_chat/views.py
+++ backend/assist_chat/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-#from chat import chat_model
-# Create your views here.
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# #from chat import chat_model
+# # Create your views here.
 
-def chat (request):
-    return HttpResponse("Hello world!")
+# def chat (request):
+#     return HttpResponse("Hello world!")
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from chat import chat_model
+
+@csrf_exempt
+def chat(request):
+    if request.method == 'POST':
+        chatbot = chat_model()
+
+        chatbot.start_conversation()
+
+        message = request.POST.get('message', '')
+
+        response = chatbot.converse(message)
+
+        return JsonResponse({'message': response})
+
+    return JsonResponse({})
